02cd4e40c186d9faaa4c900beb3dc00adf039f6e
Sol Park <solstheman@gmail.com>
2017-01-24 20:46:47 -0500

    added alert

---
+++ giphy_dog.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+	alert("here");
+	request = new XMLHttpRequest;
+	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cute+puppy', true);
+	
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400){
+			data = JSON.parse(request.responseText).data.image_url;
+			console.log(data);
+			document.getElementById("dog_gif").innerHTML = '<center><img src = "'+data+'"  title="GIF via Giphy"></center>';
+		} else {
+			console.log('reached giphy, but API returned an error');
+		 }
+	};
+
+	request.onerror = function() {
+		console.log('connection error');
+	};
+
+	request.send();
+});
