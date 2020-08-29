f7ac90e7e08a8e8223165e8bfb0fe69452db79ef
Solomon Park <solomon.m.park@gmail.com>
2020-08-29 10:24:03 -0400

    Removed parallax, dismantled sticky header

--- wedding/home.js
+++ wedding/home.js
@@ -47,17 +47,4 @@ var $window   = $(window),
 		}
 		$window.scrollTop(scrollHeight);
 	});
-  }
-
-  function sticky(){
-	var scrollTop = $window.scrollTop();
-	if (scrollTop > (height - $('#nav_wrap').height())) {
-	  $('#nav_wrap').addClass('sticky');
-	  $('#nav_wrap').removeClass('not_sticky');
-	} else {
-	  $('#nav_wrap').removeClass('sticky');
-	  $('#nav_wrap').addClass('not_sticky');
-	}
   } 
-  $window.on('touchmove', sticky);
-  $window.on('scroll', sticky);  
